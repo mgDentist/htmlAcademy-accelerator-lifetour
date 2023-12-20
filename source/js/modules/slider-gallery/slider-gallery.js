bae7c4f24596b5607dc12c14a58f68f79c57e4bc
mgDentist <gmi1987@gmail.com>
2023-12-20 14:01:11 +0400

    Добавляет секции Фотогалерея и Форму + стили на все экраны

---
+++ source/js/modules/slider-gallery/slider-gallery.js
@@ -0,0 +1,30 @@
+import Swiper from '../../vendor/swiper';
+
+export const gallerySlider = new Swiper('.gallery__slider', {
+  navigation: {
+    nextEl: '.gallery__slider-button--next',
+    prevEl: '.gallery__slider-button--prev',
+  },
+  // slidesPerView: 'auto',
+  loop: true,
+
+  breakpoints: {
+    1440: {
+      initialSlide: 0,
+      slidesPerView: 5,
+      spaceBetween: 5,
+    },
+
+    768: {
+      initialSlide: 0,
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+
+    320: {
+      initialSlide: 0,
+      slidesPerView: 2,
+      spaceBetween: 3,
+    },
+  },
+});
