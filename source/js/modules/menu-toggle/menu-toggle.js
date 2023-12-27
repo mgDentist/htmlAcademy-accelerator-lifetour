3b764525ddfbf52979f808d3689fa973b1219577
mgDentist <gmi1987@gmail.com>
2023-12-27 13:00:24 +0400

    Дополняет мобильное меню + валидация формы

--- source/js/modules/menu-toggle/menu-toggle.js
+++ source/js/modules/menu-toggle/menu-toggle.js
@@ -1,29 +1,48 @@
 export const menuOpener = () => {
   const menuToggleButton = document.querySelector('[data-button="menu-toggle"]');
-  // const headerNav = document.querySelector('[data-nav="header-nav"]');
   const headerInnerContainer = document.querySelector('.header__inner-container');
   const headerRightWrapper = document.querySelector('.header__right-wrapper');
   const headerTel = document.querySelector('.header__tel');
   const headerLogo = document.querySelector('.header__logo');
   const headerNav = document.querySelector('.header__nav');
+  const navLinks = document.querySelectorAll('.nav-list__link');
+  const body = document.body;
+  const overlay = document.querySelector('[data-burger="overlay"]');
 
+
+  const openMenu = () => {
+    headerInnerContainer.classList.add('header__inner-container--mobile-menu-opened');
+    headerTel.classList.add('header__tel--mobile-menu-opened');
+    menuToggleButton.classList.add('header__button--mobile-menu-opened');
+    headerLogo.classList.add('header__logo--mobile-menu-opened');
+    headerNav.classList.add('header__nav--mobile-menu-opened');
+    headerRightWrapper.classList.add('header__right-wrapper--mobile-menu-opened');
+    body.style.overflow = 'hidden';
+    overlay.classList.add('is-active');
+  };
+
+  const closeMenu = () => {
+    headerInnerContainer.classList.remove('header__inner-container--mobile-menu-opened');
+    headerTel.classList.remove('header__tel--mobile-menu-opened');
+    menuToggleButton.classList.remove('header__button--mobile-menu-opened');
+    headerLogo.classList.remove('header__logo--mobile-menu-opened');
+    headerNav.classList.remove('header__nav--mobile-menu-opened');
+    headerRightWrapper.classList.remove('header__right-wrapper--mobile-menu-opened');
+    body.style.overflow = '';
+    overlay.classList.remove('is-active');
+  };
 
   menuToggleButton.addEventListener('click', () => {
     if (!headerInnerContainer.classList.contains('header__inner-container--mobile-menu-opened')) {
-      headerInnerContainer.classList.add('header__inner-container--mobile-menu-opened');
-      headerTel.classList.add('header__tel--mobile-menu-opened');
-      menuToggleButton.classList.add('header__button--mobile-menu-opened');
-      headerLogo.classList.add('header__logo--mobile-menu-opened');
-      headerNav.classList.add('header__nav--mobile-menu-opened');
-      headerRightWrapper.classList.add('header__right-wrapper--mobile-menu-opened');
-
+      openMenu();
     } else {
-      headerInnerContainer.classList.remove('header__inner-container--mobile-menu-opened');
-      headerTel.classList.remove('header__tel--mobile-menu-opened');
-      menuToggleButton.classList.remove('header__button--mobile-menu-opened');
-      headerLogo.classList.remove('header__logo--mobile-menu-opened');
-      headerNav.classList.remove('header__nav--mobile-menu-opened');
-      headerRightWrapper.classList.remove('header__right-wrapper--mobile-menu-opened');
+      closeMenu();
     }
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
   });
 };
